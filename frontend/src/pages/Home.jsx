c0ab91c310e7e994ac7e0d5f07bc13c3d5a70cd4
Matthew Shu <mshu9220@uni.sydney.edu.au>
2024-04-10 18:16:57 +1000

    Added UI Kit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -45,7 +45,38 @@ function Home() {
     getNotes();
   };
 
-  return <div>Home</div>;
+  return (
+    <>
+      <div class="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col items-center justify-center">
+          <h1>Main</h1>
+          <label
+            for="my-drawer-2"
+            class="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
+        </div>
+        <div class="drawer-side">
+          <label
+            for="my-drawer-2"
+            aria-label="close sidebar"
+            class="drawer-overlay"
+          ></label>
+          <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            <h2>Sidebar</h2>
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
